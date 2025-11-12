03d577825f80a9a15034a48934dd7ef32393c20f
Jamess-Nicc <nicdao.workacc@gmail.com>
2025-11-13 01:00:01 +0800

    MMW project update 1
    The ones with the .proj are the landing page. However, if there is no proj within the filename, it is the website itself. This is for my first ever semi large scale project where I need to build a landing page and a front-end website to showcase our product. This also serves as a project for my portfolio for my proven knowledge and application of HTML, CSS, and JavaScript.

---
+++ MMW Project/mmwproj.js
@@ -0,0 +1,13 @@
+    let igButton= document.getElementById("ig");
+
+    igButton.addEventListener('click', () => {
+        window.open('https://www.instagram.com/horiseon_/?igsh=MTlpaGl0am9yYWo5ZA%3D%3D&fbclid=IwY2xjawOBhoVleHRuA2FlbQIxMQBzcnRjBmFwcF9pZAEwAAEe3MoGxf3HLGtq1Udv0FyqJvOAkFAV2pbbxTkS1YkZ4j9LchVQs3oTNlrkJzc_aem_DU852NZaL3x-xHlpxxbcNg')
+    })
+
+    let fbButton =document.getElementById("fb");
+    fbButton.addEventListener('click', () => {
+        window.open('https://www.facebook.com/profile.php?id=61583418101923&mibextid=wwXIfr&rdid=vFob3HQ2Y0vALQ7G&share_url=https%3A%2F%2Fwww.facebook.com%2Fshare%2F17RTpkCQzP%2F%3Fmibextid%3DwwXIfr#')
+    })
+
+
+
